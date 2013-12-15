8090848c2cddc78ef5d3cc5471720b98443691ab
Sean Rose <sean1rose@gmail.com>
2013-12-14 18:33:12 -0800

    Added summary of Third Greatest

---
+++ thirdGreatest.js
@@ -0,0 +1,12 @@
+// STEPS:
+// 1. Create a prelim function that's used to sort the array in descending order, based on string length
+// 2. Sort the user input based on the prelim function
+// 3. Access the 3rd value of the sorted array
+
+function prelim (a, b) {
+	return b.length - a.length						
+	}
+	
+function thirdGreatest (strArr) {
+	return strArr.sort(prelim)[2]
+	}
