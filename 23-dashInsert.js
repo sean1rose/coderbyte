c2856f15a4f4dc3414f27daa79d97594c568d5bb
Sean Rose <sean1rose@gmail.com>
2013-12-16 12:05:25 -0800

    Summarize with steps

--- 23-dashInsert.js
+++ 23-dashInsert.js
@@ -2,10 +2,10 @@
 // 1. Convert the user-input (which initially consists of just numbers) to a string
 // 2. Create a VAR1 to store the end result (this can be an array or string)
 // 3. Create a for loop to run thru each number of the user-input; for each number..
-//          - add the current # to VAR1
-//          - if the current # AND (the # immediately subsequent) are both odd, then...
-//                  -> add a '-' to VAR1
-// 4.   Return VAR1.
+//    - add the current # to VAR1
+//    - if the current # AND (the # immediately subsequent) are both odd, then...
+//      -> add a '-' to VAR1
+// 4. Return VAR1.
 // * If used an array to store the end result, then as the very very 1st step, convert the entire user-input to a string AND split based on ('') and store in a VAR. Also, in step 4, you'll need to join VAR1 using (''). Moral of the story is that you cannot use a for-loop to run thru just numbers. The for-loop will only be able to run thru a string or an array (so need to convert user-inputted #s to a string or conver to a string and then store in an array)
 
 // CODE:
