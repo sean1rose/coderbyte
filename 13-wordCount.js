2ff3da16e395378e4081c3fe7743f993f3eaeaf0
Sean Rose <sean1rose@gmail.com>
2013-12-16 19:47:34 -0800

    Provide explanation

---
+++ 13-wordCount.js
@@ -0,0 +1,8 @@
+// STEPS:
+// 1. Split the user-inputted string into component words (split it based on a space aka ' '). The split method will return an array
+// 2. Return the length of the array, which in this case, will return the # of elements in the array aka the # of words.
+
+function WordCount(str) {
+  var words = str.split(' ');
+  return words.length;
+}
