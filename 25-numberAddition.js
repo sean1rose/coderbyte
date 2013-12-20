17e9a701d57c6ba3f1e49d958093df18a53b00d5
Sean Rose <sean1rose@gmail.com>
2013-12-19 21:09:51 -0800

    Summarize with steps

--- 25-numberAddition.js
+++ 25-numberAddition.js
@@ -3,7 +3,7 @@
 // 2. Create a variable (VAR2) to hold each # contained in the parameter/string (this is so that we can take into account #s with multiple digits aka #s > 9 aka 10, 11, etc.)
 // 3. Create a for-loop that runs thru each character of the parameter-string. For each character...
 //  - If the character is a # -> add it to VAR2
-//  - Otherwise, if the character is not a # and VAR2 is not blank...
+//  - Otherwise, if the character is not a # and VAR2 is not empty... (this would happen as soon as VAR2 has at least 1 # and then a non-number is encountered)
 //    -> parse VAR2 as an integer and add it to VAR1
 //    -> reset VAR2
 // 4. If, after running thru the for-loop, VAR2 still has a value (this would happen if there were a # at the very end of the parameter-string) -> add VAR2 to VAR1 as an integer
@@ -15,8 +15,8 @@ function NumberAddition(str) {
   var curNum = '';
   for (var i = 0; i < str.length; i++) {
     if (str[i] >= '0' && str[i] <= '9') {
-      curNum += str[i];
-    } else if (curNum != '') {                  // As soon as there's a character in curNum/ as soon as curNum is no longer empty....
+      curNum += str[i];                         // The curNum string is filled with numbers. The else-branch that follows does not apply until a non-number breaks up any string of consecutive numbers (so if we had 123x, then 1 then 12 then 123 woud be in curNum and then the x would break it up and result in curNum being added to sum)
+    } else if (curNum != '') {                  // As soon as curNum is no longer empty (this else branch does not trigger until a non-number is encountered)....
       sum += parseInt(curNum);                  // -> ...convert whatever is in curNum to an integer (could be multiple digits) and add to sum
       curNum = '';                              // Reset curNum to blank (allowing you to add further numbers later on in the for loop)
     }
